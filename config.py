d74f6233e8e21dc8c2cc853cfdde3296ba1fd73f
il00li <yl99383899@gmail.com>
2025-08-26 08:20:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
 import os
 
-BOT_TOKEN = os.getenv('BOT_TOKEN', '8324471840:AAFqTHWy4-FZFIHGusm5RWk1Y240cV32SCw')
-ADMIN_ID = int(os.getenv('ADMIN_ID', 6689435577))
-CHANNEL_USERNAME = os.getenv('CHANNEL_USERNAME', '@iIl337')
-MAIN_CHANNEL = os.getenv('MAIN_CHANNEL', '@GRABOT7')
-DEVELOPER_USERNAME = os.getenv('DEVELOPER_USERNAME', '@OlIiIl7')
-
-# مسارات ملفات التخزين
-USERS_FILE = "users.json"
-SEARCH_TYPES_FILE = "search_types.json"
-INVITES_FILE = "invites.json"
+TOKEN = "8324471840:AAFqTHWy4-FZFIHGusm5RWk1Y240cV32SCw"
+OWNER_ID = 6689435577  # معرف المطور @OlIiIl7
+CHANNEL_ID = "@iIl337"  # القناة للاشتراك الإجباري
+CHANNEL_URL = "https://t.me/iIl337"
+BOT_USERNAME = "your_bot_username"
