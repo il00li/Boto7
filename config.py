4e3271dcf06305dd8c6dffd49d82533ffa01e6b3
il00li <yl99383899@gmail.com>
2025-08-26 20:06:31 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-TOKEN = "8324471840:AAFqTHWy4-FZFIHGusm5RWk1Y240cV32SCw"
-OWNER_ID = 6689435577  # معرف المطور @OlIiIl7
-CHANNEL_ID = "@iIl337"  # القناة للاشتراك الإجباري
-CHANNEL_URL = "https://t.me/iIl337"
-BOT_USERNAME = "your_bot_username"
